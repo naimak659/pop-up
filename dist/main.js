6ff587bb85e54a19162b6c990ffe6918e6eb2369
Naim AK <naimak659@gmail.com>
2024-03-14 20:27:08 +0600

    testing

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,10 @@
-let pop = document.getElementById('pop')
-let popBox= document.getElementById('popBox')
-let close = document.getElementById('close')
+let pop = document.getElementById("pop");
+let popBox = document.getElementById("popBox");
+let close = document.getElementById("close");
 
-pop.addEventListener("click", e=> {
-    popBox.classList.toggle("hidden")
-})
-close.addEventListener("click", e=> {
-    popBox.classList.toggle("hidden")
-})
+pop.addEventListener("click", (e) => {
+  popBox.classList.toggle("hidden");
+});
+close.addEventListener("click", (e) => {
+  popBox.classList.toggle("hidden");
+});
