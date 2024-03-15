3a75f013af7de637a65c249e8d2fb3b79d252720
Naim AK <naimak659@gmail.com>
2024-03-15 14:38:33 +0600

    Dark mood feature added

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,27 @@
 let pop = document.getElementById("pop");
 let popBox = document.getElementById("popBox");
 let close = document.getElementById("close");
+let theme = document.querySelector("#thememode");
+let themeSpan = document.querySelector("#thememode span");
+let main = document.querySelector("body");
+console.log(themeSpan);
 
-pop.addEventListener("click", (e) => {
+function hidden1() {
   popBox.classList.toggle("hiddenbox");
-});
-close.addEventListener("click", (e) => {
-  popBox.classList.toggle("hiddenbox");
-});
+}
+
+function themeMode() {
+  main.classList.toggle("dark");
+  if (main.classList.length == 1) {
+    themeSpan.innerHTML = "LHT";
+  } else {
+    themeSpan.innerHTML = "DRK";
+  }
+}
+
+pop.addEventListener("click", hidden1);
+close.addEventListener("click", hidden1);
+
+theme.addEventListener("click", themeMode);
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./dist/*.{html,js}"],
-    theme: {
-      extend: {
-        fontFamily: {
-          kalam: ['Kalam', 'cursive'],
-        },
+  darkMode: "selector",
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kalam: ["Kalam", "cursive"],
+      },
+      colors: {
+        csDark: "#070F2B",
+        csBG: "#27374D",
+        csBTN: "#9DB2BF",
+        csText: "#DDE6ED",
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
